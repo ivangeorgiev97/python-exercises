3ee2b8f2973c546c1b621791855634fd01387e47
ivangeorgiev97 <georgiev1313@abv.bg>
2021-07-24 14:33:06 +0300

    Small update in classes exercise

--- classes-objects.py
+++ classes-objects.py
@@ -3,25 +3,27 @@
 class MainClass:
     hobby = "football"
 
-    def introduce_me():
+    def introduce_me(self):
         "Hi, my name is Ivan"
 
+
 class MyClass(MainClass):
-  def __init__(self, name, country):
-      self.name = name
-      self.country = country
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
 
-  def __str__(self):
-      return "Name: {}, Country: {}, Hobby: {}".format(self.name, self.country, MyClass.hobby)
+    def __str__(self):
+        return "Name: {}, Country: {}, Hobby: {}".format(self.name, self.country, MyClass.hobby)
 
-  def __len__(self):
-      return self.name
+    def __len__(self):
+        return self.name
 
-  def __del__(self):
-      print("object is removed!")
+    def __del__(self):
+        print("object is removed!")
 
-  def introduce_me(self):
-    print("My name is " + self.name + " and I am from " + self.country + " my hobby is " + MyClass.hobby)
+    def introduce_me(self):
+        print(f'My name is {self.name} and I am from {self.country} and hey my hobby is {MyClass.hobby}')
+
 
 myObject = MyClass("Ivan", "Bulgaria")
 
